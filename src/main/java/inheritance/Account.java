d343c29003f644e8afe8cd1da31a06cd89bdb4d1
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-15 23:32:02 +0800

    creating account abstract class

--- src/main/java/inheritance/Account.java
+++ src/main/java/inheritance/Account.java
@@ -1,5 +1,26 @@
 package inheritance;
 
-public class Account {
+public abstract class Account {
 
+    private int accountNumber;
+    private double balance;
+
+    public Account() {}
+
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+        balance = 0;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public abstract void deposit(double amount);
+
+    public abstract void withdraw(double amount);
 }
